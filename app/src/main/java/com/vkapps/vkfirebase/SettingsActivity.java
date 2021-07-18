45f41aa3c55aed3a6f287449c636f7b372fc5ee4
vicky <vigneshwaran2747@gmail.com>
2021-07-18 21:00:02 +0530

    added material io

--- app/src/main/java/com/vkapps/vkfirebase/SettingsActivity.java
+++ app/src/main/java/com/vkapps/vkfirebase/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.vkapps.vkfirebase;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void sendVerifyMail(View view) {
+    }
+
+    public void sendOTP(View view) {
     }
 }
